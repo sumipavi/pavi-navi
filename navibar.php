d235a01c38216d9dfe70d28ab5ba46fd0cc1f3dd
sumipavi <97435408+sumipavi@users.noreply.github.com>
2022-01-18 13:27:03 +0530

    Add files via upload

---
+++ navibar.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<title>Navigation Bar</title>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<nav class="navbar navbar-default">
+<section>
+<div class="topnav" id="myTopnav">
+<ul class="nav navbar-nav">
+<li class="active"><a href="#" class="active">Home</a></li>
+<li><a href="https://www.google.com/search?q=mehndi+designs&rlz=1C1SQJL_enIN964IN964&sxsrf=AOaemvLR253WEBUCw4CVw3MkLyR_RWKnyg%3A1642490556189&ei=vGrmYemIC-SY4-EPl-2TkAM&oq=me+designs&gs_lcp=Cgdnd3Mtd2l6EAEYADIGCAAQBxAeMgYIABAHEB4yBggAEAcQHjIGCAAQBxAeMgYIABAHEB4yBggAEAcQHjIGCAAQBxAeMgYIABAHEB4yBggAEAcQHjIGCAAQBxAeOgcIABBHELADOgcIABCwAxBDOgcIIxCxAhAnOgQIABAKOgcIABCxAxAKOgQIABBDOgoIABCxAxCxAxBDOgcIABCxAxBDOgUIABCABDoFCAAQsQM6CggAEIAEEIcCEBRKBQg8EgEySgQIQRgASgQIRhgAULMVWPa0AWDBzQFoAnACeACAAbgNiAGWSZIBCzAuMi4xLjctNC4ymAEAoAEByAEKwAEB&sclient=gws-wiz">Designs</a></li>
+<li><a href="https://www.instagram.com/paviii_pavi_/">Instagram</a></li>
+<li><a href="9384776193">Contact</a></li>
+</ul>
+</div>
+</section>
+</nav>
+</body>
+</html>
